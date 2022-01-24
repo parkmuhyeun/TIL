7c3f5aafe0a9681b7b2ac5d1afb2f328bc132d96
parkmuhyeun <pjhg410@gmail.com>
2022-01-24 23:26:14 +0900

    [etc] PythonWorkSpace 1.0

---
+++ etc/PythonWorkspace/practice.py
@@ -0,0 +1,176 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+# print("ㅋ"*9)
+
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not (5 > 10))
+
+# animal = "강아지"
+# name = "연탄이"
+# age = 4
+# hobby = "산책"
+# is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + "에요")
+# print("우리집 " + animal + "의 이름은 " + name + "에요")
+# print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
+# 한 문장 주석처리
+
+'''
+여러 문장 주석처리
+'''
+
+# station = "사당"
+
+# print(station + "행 열차가 들어오고 있습니다")
+
+# print(2**3) # 2^3 = 8
+# print(5%3) # 나머지 2
+# print(10%3) # 1
+# print(5//3) # 몫 1
+# print(10//3) # 3
+
+# print(10 > 3) # True
+# print(4 >= 7) # False
+# print(10 < 3) # False
+# print(5 <= 5) # True
+
+# print(3 == 3) # True
+# print(4 == 2) # False
+# print( 3 + 4 == 7) # True
+
+# print(1 != 3) # True
+# print(not (1 != 3)) # False
+
+# print ((3 > 0) and (3 < 5)) # True
+# print ((3 > 0) & (3 < 5)) # True
+
+# print((3 > 0) or (3 > 5)) # True
+# print((3 > 0) | (3 > 5)) # True
+
+# print(5 > 4 > 3) # True
+# print(5 > 4 > 7) #False
+
+# print(2 + 3 * 4) # 14
+# print((2 + 3) * 4) # 20
+# number = 2 + 3 * 4 # 14
+# print(number)
+# number = number + 2 # 16
+# print(number)
+# number += 2 # 18
+# print(number)
+# number *= 2 # 36
+# print(number)
+# number /= 2 # 18
+# print(number)
+# number -= 2 # 16
+# print(number)
+
+# number %= 5 # 1
+# print(number)
+
+# print(abs(-5)) # 5
+# print(pow(4, 2)) # 4^2 = 4*4 = 16
+# print(max(5, 12)) # 12
+# print(min(5, 12)) # 5
+# print(round(3.14)) # 3
+# print(round(4.99)) # 5
+
+# from math import *
+# print(floor(4.99)) # 내림. 4
+# print(ceil(3.14)) # 올림. 4
+# print(sqrt(16)) # 제곱근. 4
+
+# from random import *
+
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10미만의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10이하의 임의의 값 생성
+
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+
+
+# date = randint(4, 28)
+
+# print("오프라인 스터디 모임 날짜는 매월 " + str(date) +" 일로 선정되었습니다.")
+
+# sentence = '나는 소년입니다'
+# print(sentence)
+# sentence2 = "파이썬은 쉬워요"
+# print(sentence2)
+# sentence3 = """
+# 나는 소년이고,
+# 파이썬은 쉬워요
+# """
+# print(sentence3)
+
+# jumin = "990120-1234567"
+
+# print("성별 : " + jumin[7])
+# print("연 : " + jumin[0:2]) # 0 부터 2 직전까지 (0,1)
+# print("월 : " + jumin[2:4])
+# print("일 : " + jumin[4:6])
+
+# print("생년월일 : " + jumin[:6]) # 처음부터 6 직전까지
+# print("뒤 7자리 : " + jumin[7:]) # 7 부터 끝까지
+# print("뒤 7자리 (뒤에부터) : " + jumin[-7:]) # 맨 뒤에서 7번째부터 끝까지
+
+# python = "Python is Amazing"
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper())
+# print(len(python))
+# print(python.replace("Python", "Java"))
+
+# index = python.index("n")
+# print(index)
+# index = python.index("n", index + 1)
+# print(index)
+
+# print(python.find("Java")) # 없는 경우 -1
+# print(python.index("Java")) # 없는 경우 오류냄
+
+# print(python.count("n"))
