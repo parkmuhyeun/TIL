92c91f76bd98acd5c463b22e930a9871c0ecec96
parkmuhyeun <pjhg410@gmail.com>
2022-01-25 23:05:00 +0900

    [etc] PythonWorkSpace 1.1

--- etc/PythonWorkspace/practice.py
+++ etc/PythonWorkspace/practice.py
@@ -174,3 +174,402 @@
 # print(python.index("Java")) # 없는 경우 오류냄
 
 # print(python.count("n"))
+
+# print("a" + "b")
+# print("a", "b")
+
+# 방법 1
+# print("나는 %d살입니다." % 20)
+# print("나는 %s을 좋아해요." % "파이썬")
+# print("Apple 은 %c로 시작해요." % "A")
+
+# print("나는 %s살입니다." %20)
+# print("나는 %s색과 %s색을 좋아해요." % ("파란", "빨간"))
+
+# 방법 2
+# print("나는 {}살입니다.".format(20))
+# print("나는 {}색과 {}색을 좋아해요.".format("파란", "빨간"))
+# print("나는 {0}색과 {1}색을 좋아해요.".format("파란", "빨간"))
+# print("나는 {1}색과 {0}색을 좋아해요.".format("파란", "빨간"))
+
+# 방법 3
+# print("나는 {age}살이며, {color}색을 좋아해요.".format(age = 20, color = "빨간"))
+# print("나는 {age}살이며, {color}색을 좋아해요.".format(color = "빨간", age = 20))
+
+# 방법 4 (v3.6 이상~)
+# age = 20
+# color = "빨간"
+# print(f"나는 {age}살이며, {color}색을 좋아해요.")
+
+# \n : 줄바꿈
+# print("백문이 불여일견\n백견이 불여일타")
+
+# 저는 "나도코딩"입니다.
+# print("저는 '나도코딩'입니다.")
+# print('저는 "나도코딩"입니다.')
+# print("저는 \"나도코딩\"입니다.")
+# print("저는 \'나도코딩\'입니다.")
+
+# \\ : 문장 내에서 \
+# print("\\")
+
+# \r : 커서를 맨 앞으로 이동
+# print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+# print("Redd\bApple")
+
+# \t : 탭
+# print("Red\tApple")
+
+# site = "http://naver.com"
+# asite = site.replace("http://", "")
+# dotindex = asite.index(".")
+# aasite = asite[:dotindex]
+# password = aasite[:3] + str(len(aasite)) + str(aasite.count("e")) + "!"
+# print(password)
+
+# 리스트 []
+
+# 지하철 칸별로 10명, 20명, 30명
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
+
+# subway = [10, 20, 30]
+# print(subway)
+
+# subway = ["유재석", "조세호", "박명수"]
+# print(subway)
+
+# print(subway.index("조세호"))
+
+# subway.append("하하")
+# print(subway)
+
+# subway.insert(1, "정형돈")
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# subway.append("유재석")
+# print(subway)
+# print(subway.count("유재석"))
+
+# 정렬
+# num_list = [5,4,3,2,1]
+# num_list.sort()
+# print(num_list)
+
+# 순서 뒤집기
+# num_list.reverse()
+# print(num_list)
+
+# 모두 지우기
+# num_list.clear()
+# print(num_list)
+
+# 다양한 자료형 함께 사용
+# num_list = [5,2,4,3,1]
+# mix_list = ["조세호", 20, True]
+# print(mix_list)
+
+# 리스트 확장
+# num_list.extend(mix_list)
+# print(num_list)
+
+# cabinet = {3:"유재석", 100: "김태호"}
+# print(cabinet[3])
+# print(cabinet[100])
+
+# print(cabinet.get(3))
+# print(cabinet[5]) # [] 경우 없으면 error
+# print(cabinet.get(5)) # () 경우 없으면 none
+# print("hi")
+
+# print(3 in cabinet) # True
+# print(5 in cabinet) # False
+
+# cabinet = {"A-3":"유재석", "B-100": "김태호"}
+# print(cabinet["A-3"])
+# print(cabinet["B-100"])
+
+# print(cabinet)
+# cabinet["A-3"] = "김종국"
+# cabinet["C-20"] = "조세호"
+# print(cabinet)
+
+# del cabinet["A-3"]
+# print(cabinet)
+
+# key 들만 출력
+# print(cabinet.keys())
+
+# value 들만 출력
+# print(cabinet.values())
+
+# key, value 쌍으로 출력
+# print(cabinet.items())
+
+# cabinet.clear()
+# print(cabinet)
+
+
+# 튜플
+# menu = ("돈까스", "치즈까스")
+# print(menu[0])
+# print(menu[1])
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+# name, age, hobby = "김종국", 20, "코딩"
+# print(name, age, hobby)
+
+# 집합 (set)
+# 중복 안됨, 순서 없음
+# my_set = {1,2,3,3,3}
+# print(my_set) # {1, 2, 3}
+
+# java = {"유재석", "김태호", "양세형"}
+# python = set(["유재석", "박명수"])
+
+# 교집합
+# print(java & python)
+# print(java.intersection(python))
+
+# 합집합
+# print(java | python)
+# print(java.union(python))
+
+# 차집합
+# print(java - python)
+# print(java.difference(python))
+
+# python 할 줄 아는 사람이 늘어남
+# python.add("김태호")
+# print(python)
+
+# java.remove("김태호")
+# print(java)
+
+# 자료구조의 변경
+# menu = {"커피", "우유", "주스"}
+# print(menu, type(menu))
+
+# menu = list(menu)
+# print(menu, type(menu))
+
+# menu = tuple(menu)
+# print(menu, type(menu))
+
+# menu = set(menu)
+# print(menu, type(menu))
+
+
+# from random import *
+# users = range(1, 21) # 1부터 20까지 숫자를 생성
+# print(type(users))
+# users = list(users)
+
+# print(users)
+# shuffle(users)
+# print(users)
+
+# winners = sample(users, 4)
+
+# print(" -- 당첨자 발표 -- ")
+# print("치킨 당첨자: {0}".format(winners[0]))
+# print("커피 당첨자: {0}".format(winners[1:]))
+# print(" -- 축하합니다 -- ")
+
+# weather = input("오늘 날씨는 어때요? ")
+
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요.")
+
+# temp = int(input("기온은 어때요? "))
+# if 30 <= temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10 <= temp and temp < 30:
+#     print("괜찮은 날씨에요")
+# elif 0 <= temp < 10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요. 나가지 마세요")
+
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
+
+# for wating_no in range(1, 6):
+#     print("대기번호 : {0}".format(wating_no))
+
+# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index) )
+#     index -= 1;
+#     if index == 0:
+#         print("커피는 폐기처분 되었습니다.")
+
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비 되었습니다. 호출 {1} 회".format(customer, index) )
+#     index += 1
+
+# customer = "토르"
+# person = "Unknown"
+
+# while person != customer:
+#     print("{0}, 커피가 준비 되었습니다.".format(customer) )
+#     person = input("이름이 어떻게 되세요? ")
+
+# absent = [2, 5] # 결석
+# no_book = [7]
+# for student in range(1, 11): # 1~10
+#     if student in absent:
+#         continue
+#     if student in no_book:
+#         print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
+#         break;
+#     print("{0}, 책을 읽어봐".format(student))
+
+# 출석번호가 1 2 3 4, 앞에 100을 붙이기로 함
+# students = [1,2,3,4,5]
+# print(students)
+# students = [i+100 for i in students]
+# print(students)
+
+# 학생 이름을 길이로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [len(i) for i in students]
+# print(students)
+
+# 학생 이름을 대문자로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [i.upper() for i in students]
+# print(students)
+
+# from random import *
+
+# int(random() * 50) + 1
+# cnt = 0
+# for i in range(1, 51):
+#     time = randrange(1, 51)
+#     if 5 <= time <= 15:
+#         print("[0] " + str(i) + "번째 손님 (소요시간 : " + str(time) + "분)")
+#         cnt += 1
+#     else:
+#         print("[ ] " + str(i) + "번째 손님 (소요시간 : " + str(time) + "분)")
+
+# print("총 탑승 승객 : {} 분".format(cnt))
+
+# 함수
+# def open_account():
+#     print("새로운 계좌가 생성되었습니다.")
+
+# open_account()
+
+# def deposit(balance, money):
+#     print("입금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance + money))
+#     return balance + money
+
+# def withdraw(balance, money):
+#     if balance >= money:
+#         print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
+#         return balance-money
+#     else:
+#         print("출금이 완료되지 않았씁니다. 잔액은 {0} 원입니다.".format(balance))
+#         return balance
+
+# def withdraw_night(balance, money):
+#     commission = 100
+#     return commission, balance - money - commission
+
+# balance = 0
+# balance = deposit(balance, 1000)
+# # balance = withdraw(balance, 2000)
+# # balance = withdraw(balance, 500)
+# commission, balance = withdraw_night(balance, 500)
+# print("수수료 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
+
+# def profile(name, age, main_lang):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어: {2}" \
+#         .format(name, age, main_lang))
+
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
+
+# def profile(name, age = 17, main_lang = "파이썬"):
+#      print("이름 : {0}\t나이 : {1}\t주 사용 언어: {2}" \
+#          .format(name, age, main_lang))
+
+# profile("유재석")
+# profile("김태호")
+
+# def profile(name, age, main_lang):
+#     print(name, age, main_lang)
+
+# profile(name="유재석", main_lang="파이썬", age=20)
+
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이: {1}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
+
+# def profile(name, age, *language):
+#     print("이름 : {0}\t나이: {1}\t".format(name, age), end=" ")
+#     for lang in language:
+#         print(lang, end= " ")
+#     print()
+
+# profile("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+# profile("김태호", 25, "Kotlin", "Swift")
+
+# gun = 10
+
+# def checkpoint(soldiers): # 경계근무
+#     global gun
+#     gun = gun - soldiers
+#     print("함수 내] 남은 총 : {0}".format(gun))
+
+# def checkpoint_ret(gun, soldiers):
+#     gun = gun - soldiers
+#     print("함수 내] 남은 총 : {0}".format(gun))
+#     return gun
+
+# print("전체 총: {0}".format(gun))
+# # checkpoint(2)
+# gun = checkpoint_ret(gun, 2)
+# print("남은 총 : {0}".format(gun))
+
+def std_weight(height, gender):
+    if gender == "남자":
+        return height * height * 22
+    elif gender == '여자':
+        return height * height *21
+
+
+height = 175 # 키 m 단위
+gender = "남자"
+weight = round(std_weight(height / 100, gender), 2)
+print("키 {}cm {}의 표준 체중은 {}kg 입니다.".format(height, gender ,weight))
