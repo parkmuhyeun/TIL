e428f460d646ffa6471a4bf0b22802c7bc3ffc2b
parkmuhyeun <pjhg410@gmail.com>
2022-01-28 23:17:39 +0900

    [etc] PythonWorkSpace 1.2

--- etc/PythonWorkspace/practice.py
+++ etc/PythonWorkspace/practice.py
@@ -562,14 +562,295 @@
 # gun = checkpoint_ret(gun, 2)
 # print("남은 총 : {0}".format(gun))
 
-def std_weight(height, gender):
-    if gender == "남자":
-        return height * height * 22
-    elif gender == '여자':
-        return height * height *21
+# def std_weight(height, gender):
+#     if gender == "남자":
+#         return height * height * 22
+#     elif gender == '여자':
+#         return height * height *21
 
 
-height = 175 # 키 m 단위
-gender = "남자"
-weight = round(std_weight(height / 100, gender), 2)
-print("키 {}cm {}의 표준 체중은 {}kg 입니다.".format(height, gender ,weight))
+# height = 175 # 키 m 단위
+# gender = "남자"
+# weight = round(std_weight(height / 100, gender), 2)
+# print("키 {}cm {}의 표준 체중은 {}kg 입니다.".format(height, gender ,weight))
+
+# import sys
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stderr)
+
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items():
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")
+
+# 은행 대기순번표
+# 001, 002, 003, ...
+# for num in range(1, 21):
+#     print("대기번호 : " + str(num).zfill(3))
+
+# 사용자 입력으로 입력하면 어떤값이오든 string
+# answer = input("아무 값이나 입력하세요 : ")
+# print("입력하신 값은 " + answer + "입니다.")
+
+# print("{0: >10}".format(500))
+# print("{0: >+10}".format(500))
+# print("{0: >+10}".format(-500))
+# print("{0:_<+10}".format(500))
+# print("{0:,}".format(100000000000))
+# print("{0:+,}".format(100000000000))
+# print("{0:+,}".format(-100000000000))
+# print("{0:^<+30,}".format(100000000000))
+# print("{0:f}".format(5/3))
+# print("{0:.2f}".format(5/3))
+
+# 파일 입출력
+# score_file = open("score.txt", "w", encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
+
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break;
+#     print(line)
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# lines = score_file.readlines()
+# for line in lines:
+#     print(line, end="")
+
+# score_file.close()
+
+import pickle
+# profile_file = open("profile.pickle", "wb")
+# profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) # profile 에 있는 정보를 file 에 저장
+# profile_file.close()
+
+# profile_file = open("profile.pickle", "rb")
+# profile = pickle.load(profile_file) #file 에 있는 정보를 profile 에 불러오기
+# print(profile)
+# profile_file.close()
+
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+# with open("study.txt", "r", encoding="utf8") as study_file:
+#     print(study_file.read())
+
+# for i in range(1,51):
+#     with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+#         report_file.write("- {0} 주차 주간보고 -".format(i))
+#         report_file.write("\n부서 :")
+#         report_file.write("\n이름 :")
+#         report_file.write("\n업무 요약 :")
+
+# name = "마린"
+# hp = 40
+# damage = 5
+
+# print("{0} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format( \
+#         name, location, damage))
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+
+# from random import *
+
+# # 일반 유닛
+# class Unit:
+#     def __init__(self, name, hp, speed):
+#         self.name = name
+#         self.hp = hp
+#         self.speed = speed
+#         print("{0} 유닛이 생성되었습니다.".format(name))
+
+#     def move(self, location):
+#         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+#             .format(self.name, location, self.speed))
+
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+# # 공격 유닛
+# class AttackUnit(Unit):
+#         def __init__(self, name, hp, speed, damage):
+#             Unit.__init__(self, name, hp, speed)
+#             self.damage = damage
+
+#         def attack(self, location):
+#             print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+#                 .format(self.name, location, self.damage))
+
+# class Marine(AttackUnit):
+#     def __init__(self):
+#         AttackUnit.__init__(self, "마린", 40, 1, 5)
+
+#     def stimpack(self):
+#         if self.hp > 10:
+#             self.hp -= 10
+#             print("{0} : 스팀팩을 사용합니다. (HP 10 감소)".format(self.name))
+#         else:
+#             print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
+
+# class Tank(AttackUnit):
+#     seize_developed = False
+
+#     def __init__(self):
+#         AttackUnit.__init__(self, "탱크", 150, 1 ,35)
+#         self.seize_mode = False
+
+#     def set_seize_mode(self):
+#         if Tank.seize_developed == False:
+#             return
+        
+#         if self.seize_mode == False:
+#             print("{0} : 시즈모드로 전환합니다.".format(self.name))
+#             self.damage *= 2
+#             self.seize_mode = True
+#         else:
+#             print("{0} : 시즈모드를 해제합니다.".format(self.name))
+#             self.damage /= 2
+#             self.seize_mode = False
+
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
+
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]" \
+#             .format(name, location, self.flying_speed))
+
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage)
+#         Flyable.__init__(self, flying_speed)
+#     def move(self, location):
+#         self.fly(self.name, location)
+
+# class Wraith(FlyableAttackUnit):
+#     def __init__(self):
+#         FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
+#         self.clocked = False
+
+#     def clocking(self):
+#         if self.clocked == True:
+#             print("{0} : 클로킹 모드 해제합니다.".format(self.name))
+#             self.clocked = False
+#         else:
+#             print("{0} : 클로킹 모드 설정합니다.".format(self.name))
+#             self.clocked = True
+
+# def game_start():
+#     print("[알림] 새로운 게임을 시작합니다.")
+
+# def game_over():
+#     print("Player : gg")
+#     print("[Player] 님이 게임에서 퇴장하셨습니다.")
+
+
+# game_start()
+
+# m1 = Marine()
+# m2 = Marine()
+# m3 = Marine()
+
+# t1 = Tank()
+# t2 = Tank()
+
+# w1 = Wraith()
+
+# attack_units = []
+# attack_units.append(m1)
+# attack_units.append(m2)
+# attack_units.append(m3)
+# attack_units.append(t1)
+# attack_units.append(t2)
+# attack_units.append(w1)
+
+# for unit in attack_units:
+#     unit.move("1시")
+
+# Tank.seize_developed = True
+# print("[알림] 탱크 시즈 모드 개발이 완료되었습니다.")
+
+# for unit in attack_units:
+#     if isinstance(unit, Marine):
+#         unit.stimpack()
+#     elif isinstance(unit, Tank):
+#         unit.set_seize_mode()
+#     elif isinstance(unit, Wraith):
+#         unit.clocking()
+
+# for unit in attack_units:
+#     unit.attack("1시")
+
+# for unit in attack_units:
+#     unit.damaged(randint(5, 21))
+
+# game_over()
+
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
+
+    # 매물 정보 표시
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type\
+            ,self.price, self.completion_year)
+
+
+h1 = House("강남", "아파트", "매매", "10억", "2010년")
+h2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+h3 = House("송파", "빌라", "월세", "500/50", "2000년")
+
+home_list = []
+home_list.append(h1)
+home_list.append(h2)
+home_list.append(h3)
+
+print("총 {0}대의 매물이 있습니다.".format(len(home_list)))
+for home in home_list:
+    home.show_detail()
